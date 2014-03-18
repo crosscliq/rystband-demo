d2abff1bbdd33555d83a8fe2c5e39c9b000e5358
Chris French <chris@ammonitenetworks.com>
2014-03-18 11:13:17 -0600

    stuff

--- apps/Rystband/Controllers/Devices/Content.php
+++ apps/Rystband/Controllers/Devices/Content.php
@@ -16,9 +16,9 @@ class Content extends Base
 
      public function carrequest() {
         $f3 = \Base::instance();
-
+        $tagid = $f3->get('PARAMS.tagid');
         $tags = new \Rystband\Models\Tags;
-        $tags->setState('filter.tagid', $f3->get('PARAMS.tagid'));
+        $tags->setState('filter.tagid', $tagid);
         $tag = $tags->getItem();
 
         $attendees = new \Rystband\Models\Attendees;
@@ -29,6 +29,26 @@ class Content extends Base
         $pusher = new \Pusher($f3->get('pusher_key'), $f3->get('pusher_secret'), $f3->get('pusher_app_id');
         $data = array('POST' => $f3->get('POST'), 'attendee' => (array) $attendee->cast() );
         $pusher->trigger('cardisplay', 'post', $data);
+
+
+        $f3->set('tag', $tag);
+        $f3->set('tagid', $tagid);
+
+        $route = \Base::instance()->get('PARAMS[0]');
+        $peices = explode('/', $route);
+        $channel = end($peices);
+       
+        $f3->set('channel', $channel);
+      
+        if(empty($tag->attendee)) {
+            $f3->set('showselfregister', true);
+            $view = new \Dsc\Template;
+            echo $view->render('Msft/Views::attendee/own.php');
+        } else {
+             $f3->set('showselfregister', false);
+            $view = new \Dsc\Template;
+            echo $view->render('Msft/Views::attendee/own.php');
+        }
 
     }
 
