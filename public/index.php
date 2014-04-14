e60ee0b44da3f2ac924080c5bfe092477047b25e
root <root@msft.cc>
2014-04-14 18:28:03 +0000

    everything

--- public/index.php
+++ public/index.php
@@ -85,6 +85,45 @@ $db_name = \Base::instance()->get('db.mongo.name');
 $db = new \DB\Mongo('mongodb://localhost:27017', $db_name);
 new \DB\Mongo\Session($db);
 */
+$app->set('UPLOADS','uploads/'); // don't forget to set an Upload directory, and make it writable!
+
+$app->route('POST /uploadtest',
+    function() {
+	$overwrite = false; // set to true, to overwrite an existing file; Default: false
+$slug = true; // rename file to filesystem-friendly version
+$web = \Web::instance();
+	
+	$files = $web->receive(function($file){
+        var_dump($file);
+        /* looks like:
+          array(5) {
+              ["name"] =>     string(19) "csshat_quittung.png"
+              ["type"] =>     string(9) "image/png"
+              ["tmp_name"] => string(14) "/tmp/php2YS85Q"
+              ["error"] =>    int(0)
+              ["size"] =>     int(172245)
+            }
+        */
+        // $file['name'] already contains the slugged name now
+
+        // maybe you want to check the file size
+        if($file['size'] > (2 * 1024 * 1024)) // if bigger than 2 MB
+            return false; // this file is not valid, return false will skip moving it
+
+        // everything went fine, hurray!
+        return true; // allows the file to be moved from php tmp dir to your defined upload dir
+    },
+    $overwrite,
+    $slug
+);
+
+    }
+);
+
+
+
+
+
 
 $app->run();
 
