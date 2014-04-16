53746f3dd37bf55bbade1f41319579f43b4cb4b9
Chris French <chris@ammonitenetworks.com>
2014-04-16 15:33:14 -0600

    Photo Booth

--- apps/Rystband/Controllers/Devices/Photo.php
+++ apps/Rystband/Controllers/Devices/Photo.php
@@ -15,7 +15,7 @@ class Photo extends Base
     }
 
     public function processImage() {
-
+            $f3 = \Base::instance();
 
             $route = \Base::instance()->get('PARAMS[0]');
             $peices = explode('/', $route);
@@ -32,30 +32,54 @@ class Photo extends Base
             $overwrite = true; // set to true, to overwrite an existing file; Default: false
             $slug = true; // rename file to filesystem-friendly version
             
-            $web = \Web::instance();
-    
-            $files = $web->receive(function($file){
-                // maybe you want to check the file size
-                if($file['size'] > (2 * 1024 * 1024)) // if bigger than 2 MB
-                    return false; // this file is not valid, return false will skip moving it
+            if($f3->set('POST.begin')) {
 
-                // everything went fine, hurray!
-                return true; // allows the file to be moved from php tmp dir to your defined upload dir
-            },
-            $overwrite,
-            $slug
-        );
-            $photos = array();
-            if(is_array($attendee->photos)) {
-                foreach ($attendee->photos as $key => $photo) {
-                   $photos[] = $photo;
-                }
+                $pusher = new \Pusher($item->{'pusher.public'}, $item->{'pusher.private'}, $item->{'pusher.app_id'});
+
+            $data = array('attendee' => $attendee->cast());
+
+            $pusher->trigger('photobooth', 'begin', $data);
+
+
+
+            } else {
+
+
+                  $web = \Web::instance();
+        
+                    $files = $web->receive(function($file){
+                        // maybe you want to check the file size
+                        if($file['size'] > (2 * 1024 * 1024)) // if bigger than 2 MB
+                            return false; // this file is not valid, return false will skip moving it
+
+                        // everything went fine, hurray!
+                        return true; // allows the file to be moved from php tmp dir to your defined upload dir
+                    },
+                    $overwrite,
+                    $slug
+                );
+
+                    $photos = array();
+                    if(is_array($attendee->photos)) {
+                        foreach ($attendee->photos as $key => $photo) {
+                           $photos[] = $photo;
+                        }
+                    }
+                     $photos[] = key($files);
+                    $attendee->photos = $photos;
+
+                    $attendee->save();
+
+                     $pusher = new \Pusher($item->{'pusher.public'}, $item->{'pusher.private'}, $item->{'pusher.app_id'});
+
+            $data = array('attendee' => $attendee->cast(), 'photo' =>  key($files));
+            
+            $pusher->trigger('photobooth', 'photo', $data);
+
             }
-             $photos[] = key($files);
-            $attendee->photos = $photos;
 
-            $attendee->save();
 
+          
             
             
 
