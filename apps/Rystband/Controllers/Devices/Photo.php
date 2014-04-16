d599074f58431394d2b072c05fe245471d9298d3
Chris French <chris@ammonitenetworks.com>
2014-04-16 13:32:28 -0600

    d

--- apps/Rystband/Controllers/Devices/Photo.php
+++ apps/Rystband/Controllers/Devices/Photo.php
@@ -36,20 +36,6 @@ class Photo extends Base
             $web = \Web::instance();
     
             $files = $web->receive(function($file){
-       
-
-       // var_dump($file);
-        /* looks like:
-          array(5) {
-              ["name"] =>     string(19) "csshat_quittung.png"
-              ["type"] =>     string(9) "image/png"
-              ["tmp_name"] => string(14) "/tmp/php2YS85Q"
-              ["error"] =>    int(0)
-              ["size"] =>     int(172245)
-            }
-        */
-        // $file['name'] already contains the slugged name now
-
                 // maybe you want to check the file size
                 if($file['size'] > (2 * 1024 * 1024)) // if bigger than 2 MB
                     return false; // this file is not valid, return false will skip moving it
@@ -60,8 +46,6 @@ class Photo extends Base
             $overwrite,
             $slug
         );
-
-    });
 
 
             $attendee->authkey = rand();
